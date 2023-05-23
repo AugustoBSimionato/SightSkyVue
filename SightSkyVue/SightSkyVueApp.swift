fb04aadc0b4088cd2d6c71f5fdb75853a3d9133d
AugustoBSimionato <guto.baroni@gmail.com>
2023-05-23 10:19:36 -0300

    Initial Commit

---
+++ SightSkyVue/SightSkyVueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightSkyVueApp.swift
+//  SightSkyVue
+//
+//  Created by Augusto Simionato on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SightSkyVueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
